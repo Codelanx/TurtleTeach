e65ca1f0ca61c72311517af574c8ed6e66d283d5
Spencer Alderman <rogue@codelanx.com>
2021-04-10 16:47:20 -0400

    Fix merge

--- src/pages/Testing.js
+++ src/pages/Testing.js
@@ -14,19 +14,18 @@ class Testing extends React.Component {
             <List>
                 <ListGroup>
                     <ListGroupItem>
-                        <Link to="/landing">Landing Page</Link><br/>
-                        <Link to="/login">Login Page</Link><br/>
-                        <Link to="/home">Home Page</Link><br/>
-                        <Link to="/course">Course Page</Link><br/>
-                        <Link to="/customize">Customize Page</Link><br/>
-                        <Link to="/sandbox">Sandbox Page</Link><br/>
                         <Link to="/test/component">Component Testing Page</Link>
                     </ListGroupItem>
                 </ListGroup>
                 <hr />
                 <ListGroup>
                     <ListGroupItem>
-                        <Link to="/landing">Landing Page</Link>
+                        <Link to="/landing">Landing Page</Link><br/>
+                        <Link to="/login">Login Page</Link><br/>
+                        <Link to="/home">Home Page</Link><br/>
+                        <Link to="/course">Course Page</Link><br/>
+                        <Link to="/customize">Customize Page</Link><br/>
+                        <Link to="/sandbox">Sandbox Page</Link><br/>
                     </ListGroupItem>
                 </ListGroup>
             </List>
