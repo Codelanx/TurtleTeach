e47a9e3bb56cab6c4503db573e559bc61779313a
Brandon Cunningham <btc2628@rit.edu>
2021-04-10 16:17:23 -0400

    Button Component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,48 +2,43 @@ import React from "react";
 import './Navigation.css';
 import {Badge, Button, Input, InputGroup, Nav, NavItem, NavLink} from "reactstrap";
 
-class Button extends React.Component {
+class TurtleButton extends React.Component {
 
     constructor(props) {
         super(props);
     }
 
+
     render() {
-        //TODO: Remove below example code after navbar up and running
-        /*return (
-            <React.Fragment>
-                <Button id="nav-fold" type="button"
-                        className="btn btn-secondary btn-lg btn-block col-xs-12 d-block d-md-none" data-toggle="collapse"
-                        data-target="#sidenav" aria-expanded="true" aria-controls="collapseExample">
-                    <span className="fa fa-bars"></span>
-                </Button>
-                <div className="col-md-3 col-lg-2 col-xs-12 sidebar collapse" id="sidenav">
-                    <Nav vertical={true} className="nav-sidebar flex-column">
-                        <NavItem><NavLink href="./queue">Queue <Badge color="secondary">{this.queueSize}</Badge></NavLink></NavItem>
-                        <NavItem><NavLink href="./browser">Browser</NavLink></NavItem>
-                        <NavItem><NavLink href="./admin">Admin</NavLink></NavItem>
-                        <NavIteTm><NavLink href="./torrents">Torrents</NavLink></NavItem>
-                        <NavItem><NavLink href="./logout">Logout</NavLink></NavItem>
-                    </Nav>
-                    <div className="control-row">
-                        <InputGroup className="searcher col-xs-12" data-search-type="general">
-                            <Input type='text' placeholder='Search media...' />
-                            <div className="form-popup d-none"></div>
-                        </InputGroup>
-                    </div>
-                </div>
-            </React.Fragment>
-        );*/
+
+        const buttonStyle = {
+            backgroundColor: this.props.color,
+            color: '#AF11FF',
+            boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+            border: "none",
+            borderRadius: "80px",
+            fontSize: "11px",
+            outline: "none",
+            marginLeft: this.props.marginLeft,
+            width : this.props.width,
+            height : this.props.height
+        };
+
+        function changeBackground(e) {
+            e.target.style.background = '#6E00A5';
+        }
+
+        function originalBackground(e) {
+            e.target.style.background = '#AF11FF';
+        }
+
         return (
-            <div>
+            <TurtleButton onMouseOver={changeBackground} onMouseLeave={originalBackground} style={buttonStyle} type="button" value={this.props.value}>
 
-                <Nav pills>
-
-                </Nav>
-            </div>
+            </TurtleButton>
         );
     }
 
 }
 
-export default Button;
+export default TurtleButton;
