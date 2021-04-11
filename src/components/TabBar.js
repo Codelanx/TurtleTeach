4e0d806a7a88a28a34cb54b711b5216625e09ded
Brandon Cunningham <btc2628@rit.edu>
2021-04-11 11:34:40 -0400

    Filled out Tab Bar

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,23 +1,72 @@
-import React from "react";
+import React, {useState} from "react";
 import './TabBar.css';
-import {Button} from "reactstrap";
 import {Link} from "react-router-dom";
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, CardTitle, CardText, Row, Col } from 'reactstrap';
 
-class TabBar extends React.Component {
+const TabBar = (props) => {
+    const [activeTab, setActiveTab] = useState('1');
 
-    constructor(props) {
-        super(props);
-
+    const toggle = tab => {
+        if(activeTab !== tab) setActiveTab(tab);
     }
 
-    render() {
-        return (
-            <div>
-                {this.props.text}
-            </div>
-        );
-    }
+    return (
+        <div>
+            <Nav tabs className="nav-fill">
+                <NavItem>
+                    <NavLink
+                        onClick={() => { toggle('1'); }}
+                    >
+                        My Assignments
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
 
+                        onClick={() => { toggle('2'); }}
+                    >
+                        Class Leaderboard
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+
+                        onClick={() => { toggle('3'); }}
+                    >
+                        My Grades
+                    </NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink
+
+                        onClick={() => { toggle('4'); }}
+                    >
+                        Tutorial
+                    </NavLink>
+                </NavItem>
+            </Nav>
+            <TabContent activeTab={activeTab}>
+                <TabPane tabId="1">
+                    <Row>
+                        Assignments Page Goes Here
+                    </Row>
+                </TabPane>
+                <TabPane tabId="2">
+                    <Row>Class Leaderboard Goes Here
+                    </Row>
+                </TabPane>
+                <TabPane tabId="3">
+                    <Row>
+                        Grades Page Goes Here
+                    </Row>
+                </TabPane>
+                <TabPane tabId="4">
+                    <Row>Here Shall Be Tutorials Page
+                    </Row>
+                </TabPane>
+            </TabContent>
+        </div>
+    );
 }
 
 export default TabBar;
