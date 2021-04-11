fb43207fa9d1ac1662e4bdd52f3f52462d420345
Spencer Alderman <rogue@codelanx.com>
2021-04-11 11:22:48 -0400

    add User data class

---
+++ src/User.js
@@ -0,0 +1,67 @@
+class UserType {
+    static STUDENT = 0;
+    static TEACHER = 1;
+    static CREATOR = 2;
+}
+
+class User {
+
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    getUsername() {
+        return this.name;
+    }
+
+    getUserType() {
+        return this.type;
+    }
+
+    isStudent() {
+        return this.getUserType() === UserType.STUDENT;
+    }
+
+    isTeacher() {
+        return this.getUserType() === UserType.TEACHER;
+    }
+
+    isCreator() {
+        return this.getUserType() === UserType.CREATOR;
+    }
+
+}
+
+class Users {
+    static STUDENT = new User("Student", UserType.STUDENT);
+    static TEACHER = new User("Teacher", UserType.TEACHER);
+    static CREATOR = new User("Creator", UserType.CREATOR);
+
+    static findUser(username) {
+        switch (username.toLowerCase()) {
+            case "student": return Users.STUDENT;
+            case "teacher": return Users.TEACHER;
+            case "creator": return Users.CREATOR;
+        }
+        return null;
+    }
+}
+
+class Profile {
+
+    constructor() {
+        this.currentUser = null;
+    }
+
+    getCurrentUser() {
+        return this.currentUser;
+    }
+
+    setCurrentUser(username) {
+        this.currentUser = Users.findUser(username);
+    }
+
+}
+
+export default Profile;
