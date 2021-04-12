bce59719592b02d9b3ffcc48cf3936e08e76f04d
ajm9099 <ajm9099@rit.edu>
2021-04-12 17:03:42 -0400

    Add base PuzzleSolver files

---
+++ src/pages/PuzzleSolver/PuzzleSolver.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './PuzzleSolver.css';
+
+class PuzzleSolver extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Sup, bitch?
+            </div>
+        );
+    }
+
+}
+
+export default PuzzleSolver;
