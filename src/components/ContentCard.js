c7eeaec68369dba356d27f641361459b8493b904
Ryan Lemole <rpl9238@g.rit.edu>
2021-04-12 14:32:16 -0400

    logos are uploaded as logo1.png and logo2.png in /img directory

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './ContentCard.css';
-import {Card, CardImg} from "reactstrap";
+import {Card, CardImg, Button} from "reactstrap";
 
 class ContentCard extends React.Component {
 
@@ -21,10 +21,8 @@ class ContentCard extends React.Component {
                     <div id="BottomTextClass">
                         <div>
                             <a href="#top" id="link">Introduction to TurtleTeach</a>
+                            <Button>Grade: B+</Button>
                         </div>
-                    </div>
-                    <div>
-                        Grade: B+
                     </div>
                     <div>
                         *Unsure how to shrink image/card*
