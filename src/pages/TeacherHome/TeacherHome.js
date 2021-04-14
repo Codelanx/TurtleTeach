d80a867046926da915648bfada5cb68e7cbe5cc2
Brandon Cunningham <btc2628@rit.edu>
2021-04-14 12:00:14 -0400

    made less meme

--- src/pages/TeacherHome/TeacherHome.js
+++ src/pages/TeacherHome/TeacherHome.js
@@ -19,7 +19,7 @@ class TeacherHome extends React.Component {
                     <Button>Add New Class</Button>
                 </Row>
                 <Row>
-                    <CardLayout topText={"Current Teaching"} class1={"Turtle Racing"} date1={"Jan-Jun 2021"} notifications1={"10"} class2={"Turtle Ethics"} date2={"Jan-Jun 2021"} notifications2={"69"}
+                    <CardLayout topText={"Current Teaching"} class1={"Turtle Racing"} date1={"Jan-Jun 2021"} notifications1={"10"} class2={"Turtle Ethics"} date2={"Jan-Jun 2021"} notifications2={"50"}
                                 bottomText={"Previous Taught"} class3={"Introduction"} date3={"Aug 2020"} class4={"Control Statements"} date4={"Aug-Dec 2020"} class5={"Shell Dynamics"} date5={"Aug-Dec 2020"}
                                  class6={"Turtle Ethics"} date6={"Aug-Dec 2020"} link={"/ClassHome"}/>
                 </Row>
