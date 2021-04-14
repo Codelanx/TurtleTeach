a63a66d22805faeab817de92fe306667cdd5dbed
Brandon Cunningham <btc2628@rit.edu>
2021-04-14 09:17:36 -0400

    Added Teacher grade collapsing

--- src/components/TeacherGrades.js
+++ src/components/TeacherGrades.js
@@ -1,6 +1,33 @@
 import React from "react";
 import './TeacherAssignment.css';
-import {Table} from "reactstrap";
+import {Row, Col, UncontrolledCollapse} from "reactstrap";
+
+function PuzzleListing(props) {
+    return (
+        <React.Fragment>
+            <Row className={"assignments-course"}>
+                <Col xs={"6"}><a href="#" id={props.name}>{props.name}</a></Col>
+                <Col xs={"2"}>{props.completed}</Col>
+                <Col xs={"2"}>{props.evaluated}</Col>
+                <Col xs={"2"}>{props.date}</Col>
+            </Row>
+            <UncontrolledCollapse toggler={props.toggler} defaultOpen={"True"}>{props.children}</UncontrolledCollapse>
+
+        </React.Fragment>
+    );
+}
+
+function StudentListing(props) {
+    return (
+        <Row className={"assignments-puzzle"}>
+            <Col xs={"5"}>{props.name}</Col>
+            <Col xs={"2"}>{props.completed}</Col>
+            <Col xs={"2"}>{props.evaluated}</Col>
+            <Col xs={"2"}>{props.date}</Col>
+        </Row>
+    );
+}
+
 
 class TeacherGrades extends React.Component {
 
@@ -12,48 +39,58 @@ class TeacherGrades extends React.Component {
 
 
         return (
-            <Table bordered>
-                <thead>
-                <tr>
-                    <th>Assignment</th>
-                    <th>Completed</th>
-                    <th>Evaluated</th>
-                    <th>Due Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr bgcolor="#00FF00">
-                    <td>Swagatoni</td>
-                    <td>3/4</td>
-                    <td>2/4</td>
-                    <td>04/20/21</td>
-                </tr>
-                <tr bgcolor="#bbbbbb">
-                    <td><a href={"/DevEnvironment"} id="link" >User01</a></td>
-                    <td>Yes</td>
-                    <td>Published</td>
-                    <td>On-Time</td>
-                </tr>
-                <tr bgcolor="#bbbbbb">
-                    <td><a href={"/DevEnvironment"} id="link" >User02</a></td>
-                    <td>Yes</td>
-                    <td>Draft Saved</td>
-                    <td>On-Time</td>
-                </tr>
-                <tr bgcolor="#bbbbbb">
-                    <td><a href={"/DevEnvironment"} id="link" >User03</a></td>
-                    <td>Yes</td>
-                    <td>Unread</td>
-                    <td>2 hr. late</td>
-                </tr>
-                <tr bgcolor="#bbbbbb">
-                    <td><a href={"#top"} id="link" >User04</a></td>
-                    <td>No</td>
-                    <td>Unread</td>
-                    <td>D.N.F</td>
-                </tr>
-                </tbody>
-            </Table>
+            <React.Fragment>
+                <Row className={"category"}>
+                    <Col xs={"6"}>Assignment</Col>
+                    <Col xs={"2"}>Completed</Col>
+                    <Col xs={"2"}>Evaluated</Col>
+                    <Col xs={"2"}>Due Date</Col>
+                </Row>
+                <PuzzleListing name={"Swagscape"}
+                               evaluated={"3/4"}
+                               completed={"2/4"}
+                               date={"04-13-21"}
+                                toggler={"#Swagscape"}>
+                    <StudentListing name={"Jimmy Longbottom"}
+                                    completed={"Yes"}
+                                    evaluated={"Published"}
+                                    date={"On-time"} />
+                    <StudentListing name={"Katty Brett"}
+                                    completed={"Yes"}
+                                    evaluated={"Draft Saved"}
+                                    date={"On-time"} />
+                    <StudentListing name={"Sandy Cheeks"}
+                                    completed={"Yes"}
+                                    evaluated={"Unread"}
+                                    date={"2 hr. late"} />
+                    <StudentListing name={"Lewis Yeeticus"}
+                                    completed={"No"}
+                                    evaluated={"Unread"}
+                                    date={"D.N.F"} />
+                </PuzzleListing>
+                <PuzzleListing name={"Flipster"}
+                               evaluated={"0/4"}
+                               completed={"1/4"}
+                               date={"04-25-21"}
+                                toggler={"#Flipster"}>
+                    <StudentListing name={"Jimmy Longbottom"}
+                                    completed={"Yes"}
+                                    evaluated={"Unread"}
+                                    date={"On-time"} />
+                    <StudentListing name={"Katty Brett"}
+                                    completed={"No"}
+                                    evaluated={"-"}
+                                    date={"-"} />
+                    <StudentListing name={"Sandy Cheeks"}
+                                    completed={"No"}
+                                    evaluated={"-"}
+                                    date={"-"} />
+                    <StudentListing name={"Lewis Yeeticus"}
+                                    completed={"No"}
+                                    evaluated={"-"}
+                                    date={"-"} />
+                </PuzzleListing>
+            </React.Fragment>
 
         );
     }
