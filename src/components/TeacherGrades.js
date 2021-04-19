ea30ad70a69877f61612b69a6ed4500336a93ab8
Spencer Alderman <rogue@codelanx.com>
2021-04-18 21:33:14 -0400

    CSS fixes for grading/assignments

--- src/components/TeacherGrades.js
+++ src/components/TeacherGrades.js
@@ -8,11 +8,8 @@ function PuzzleListing(props) {
         <React.Fragment>
             <Row className={"assignments-course"}>
                 <Col xs={"1"} className={"Collapse"} id={props.name}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
-                         className="bi bi-arrows-collapse" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd"
-                              d="M1 8a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 8zm7-8a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 1 1 .708-.708L7.5 4.293V.5A.5.5 0 0 1 8 0zm-.5 11.707-1.146 1.147a.5.5 0 0 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 11.707V15.5a.5.5 0 0 1-1 0v-3.793z"/>
-                    </svg></Col>
+                    <i className="bi bi-arrows-collapse" />
+                </Col>
                 <Col xs={"5"}>{props.name}</Col>
                 <Col xs={"2"}>{props.completed}</Col>
                 <Col xs={"2"}>{props.evaluated}</Col>
@@ -27,7 +24,8 @@ function PuzzleListing(props) {
 function StudentListing(props) {
     return (
         <Row className={"assignments-puzzle"}>
-            <Col xs={"5"}><Link to={"/puzzle/123"} id="link">{props.name}</Link></Col>
+            <Col xs={"2"}><span>&nbsp;</span></Col>
+            <Col xs={"4"}><Link to={"/puzzle/123"} id="link">{props.name}</Link></Col>
             <Col xs={"2"}>{props.completed}</Col>
             <Col xs={"2"}>{props.evaluated}</Col>
             <Col xs={"2"}>{props.date}</Col>
