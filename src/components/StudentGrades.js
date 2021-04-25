7d1d9e580c79ae43ce46123a016f6ffaa2336268
Brandon Cunningham <btc2628@rit.edu>
2021-04-25 19:39:36 -0400

    Fixed Student Grade Page

--- src/components/StudentGrades.js
+++ src/components/StudentGrades.js
@@ -14,59 +14,31 @@ class StudentGrades extends React.Component {
     render() {
         return (
             <Row>
-                <Col xs={2}>
-                    <Table bordered>
-                        <thead>
-                        <th>Overall Grade</th>
-                        </thead>
-                        <tbody>
-                            <th className={"grade-font"}>B+</th>
-                        </tbody>
-                    </Table>
+                <Col xs={4}>
+                    <Row className={"overall"}>Overall Grade</Row>
+                    <Row className={"grade-font"}>B+</Row>
                 </Col>
                 <Col xs={8}>
-            <Table bordered>
-                <thead>
-                <tr>
-                    <th>Assignment</th>
-                    <th>Completed</th>
-                    <th>Evaluated</th>
-                    <th>Due Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr bgcolor="#00FF00">
-                    <td>Swagatoni</td>
-                    <td>3/4</td>
-                    <td>2/4</td>
-                    <td>04/20/21</td>
-                </tr>
-                <tr bgcolor="#bbbbbb">
-                    <td>User01</td>
-                    <td>Yes</td>
-                    <td>Published</td>
-                    <td>On-Time</td>
-                </tr>
-                <tr bgcolor="#bbbbbb">
-                    <td>User02</td>
-                    <td>Yes</td>
-                    <td>Draft Saved</td>
-                    <td>On-Time</td>
-                </tr>
-                <tr bgcolor="#bbbbbb">
-                    <td>User03</td>
-                    <td>Yes</td>
-                    <td>Unread</td>
-                    <td>2 hr. late</td>
-                </tr>
-                <tr bgcolor="#bbbbbb">
-                    <td>User04</td>
-                    <td>No</td>
-                    <td>Unread</td>
-                    <td>D.N.F</td>
-                </tr>
-                </tbody>
-            </Table>
+                    <Row className={"label"}>
+                        <Col xs={9}>Assignment</Col>
+                        <Col xs={3}>Grade</Col>
+                    </Row>
+                    <Row className={"assignment"}>
+                        <Col xs={9}>Turtle Slide</Col>
+                        <Col xs={3}>B+</Col>
+                    </Row>
+                    <Row className={"assignment"}>
+                        <Col xs={9}>Turtle Ride</Col>
+                        <Col xs={3}>A-</Col>
+                    </Row>
+                    <Row className={"assignment"}>
+                        <Col xs={9}>Turtle Hide</Col>
+                        <Col xs={3}>C</Col>
+                    </Row>
+                    <Row className={"assignment"}>
+                        <Col xs={9}>Turtle Died</Col>
+                        <Col xs={3}>A+</Col>
+                    </Row>
                 </Col>
             </Row>
         );
