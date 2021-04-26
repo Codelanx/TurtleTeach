3107a14c55c49ce7a0b05249705ab57b0b4d00b7
Brandon Cunningham <btc2628@rit.edu>
2021-04-25 20:02:28 -0400

    removed take teacher to classhome on submit

--- src/components/GradeSpace.js
+++ src/components/GradeSpace.js
@@ -3,22 +3,26 @@ import './GradeSpace.css';
 import {Row, Col, Button} from "reactstrap";
 import {Link} from "react-router-dom";
 
+
 class GradeSpace extends React.Component {
 
     constructor(props) {
         super(props);
     }
 
+
+
     render() {
 
         return (
+
             <Col className={"gradespace"}>
 
                 <p className={"text"}>Student: Jimmy Longbottom</p>
                 <p className={"text"}>
-                    Grade: [<input type="text" name="Grade" className={"gradeInput"} />]/100
+                    Grade: [<input id="GradeInput" type="text" name="Grade" className={"gradeInput"}/>]/100
                 </p>
-                <p className={"text"}><Button tag={Link} to="/ClassHome">Submit Grade</Button></p>
+                <p className={"text"}><Button tag={Link} to="/puzzle/124" >Submit Grade</Button></p>
             </Col>
         );
     }
