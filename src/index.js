f3aef047eef78de98fad5708c8e9ef80d92d86f8
Spencer Alderman <rogue@codelanx.com>
2021-04-26 11:05:16 -0400

    Add a basename

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from "./App"
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename={"/turtleteach/"}>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
